40ae544f5785f6397178982361c62a68dae50048
DoudGaya <arbdoule@yahoo.com>
2023-10-15 01:08:41 +0100

    cheese

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,37 +6,19 @@ import { DarkButton } from "./DarkButton"
 
 export const Navbar = () => {
     return (
-        <div className="
-            w-full flex
-            flex-col fixed
-            top-0
-            justify-center
-            space-x-2
-            dark:bg-dark-shade
-            bg-banner
-            dark:text-gray-100
-            py-2
-            border-b
-            dark:border-dark-shade-bright"
+        <div className="w-full flex flex-col fixed items-center top-0 justify-center space-x-2 dark:bg-dark-shade bg-banner dark:text-gray-100 py-2 border-b dark:border-dark-shade-bright"
         >
-            <div className="
-            max-w-6xl
-            items-center
-            mx-auto flex
-            justify-between"
+                <Link href={'/'} className=" h-20 overflow-hidden  justify-center w-full py-2 flex ">
+                        <Image alt="Nillar Logo" className="h-16  object-contain hidden dark:block " src={nillar} />
+                        <Image alt="Nillar Logo" className="h-16 block dark:hidden object-contain " src={nillarDark} />
+                </Link>
+            <div className=" items-center mx-auto border-t-2 pt-2 dark:border-dark-shade-bright border-primary-light  flex  w-full justify-center"
             >
-
-                <div className=" flex items-start">
-                    <Link href={'/'} className=" h-20 overflow-hidden py-2 flex stroke-black fill-black items-start ">
-                        <Image alt="Nillar Logo" className="h-16 block dark:hidden object-contain object-left " src={nillar} />
-                        <Image alt="Nillar Logo" className="h-16 hidden dark:block object-contain object-left " src={nillar} />
-                    </Link>
-                </div>
-                <div className=" space-x-4 flex bg-green-30 items-center flex-none">
+                <div className=" space-x-12 flex justify-center w-full relative  bg-green-30 items-center">
                     <Link href={'/how-to'} className="uppercase font-header hover:underline" >HOW TO</Link>
                     <Link href={'/news'} className="uppercase font-header hover:underline" >NEWS</Link>
                     <Link href={'/articles'} className="uppercase font-header hover:underline" >ARTICLES</Link>
-                    <div className="  flex items-start pl-10">
+                    <div className="  flex absolute right-8 items-start pl-10">
                         <DarkButton />
                     </div>
                 </div>
