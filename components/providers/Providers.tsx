3c348a1f708c7683fa8f9ec0b9cd0b2df95c2a47
DoudGaya <arbdoule@yahoo.com>
2023-12-21 15:56:34 +0100

    v2.0

--- components/providers/Providers.tsx
+++ components/providers/Providers.tsx
@@ -1,15 +1,9 @@
 "use client"
 import { ThemeProvider } from "next-themes"
-import { NavContextProvider } from "../context/MobileNavContext"
-import { SideBarProvider } from "../context/DesktopSideBarContext"
 export const Providers = ({children}: {children: React.ReactNode}) => {
     return (
         <ThemeProvider attribute="class">
-            {/* <NavContextProvider> */}
-                {/* <SideBarProvider> */}
                     {children}
-                {/* </SideBarProvider> */}
-            {/* </NavContextProvider> */}
         </ThemeProvider>
     )
 }
