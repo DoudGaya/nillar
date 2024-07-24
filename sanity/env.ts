7b88055b951bbbf31852e2b06b28112e79663c96
doudGaya <arbdoule@yahoo.com>
2024-07-24 17:15:24 +0100

    java

--- sanity/env.ts
+++ sanity/env.ts
@@ -10,7 +10,7 @@ export const projectId = assertValue(
   process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
 )
-
+ 
 export const useCdn = true
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
